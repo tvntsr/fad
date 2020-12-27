5217f5ba5b76ff017cf9569d3e4b3a7a31bf85ad
Volodymyr Tarasenko <tvntsr@yahoo.com>
2020-12-28 01:13:04 +0200

    licence, gpl v3

--- src/fanotifyerror.hpp
+++ src/fanotifyerror.hpp
@@ -1,9 +1,24 @@
 #ifndef _FAD_FANOTIFY_GROUP_ERROR_HPP
 #define _FAD_FANOTIFY_GROUP_ERROR_HPP
 
+// Copyright 2020 Volodymyr Tarasenko
+// Author : Volodymyr Tarasenko
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <string>
 #include <exception>
-
 
 /// Main error, it could be critical
 class FanotifyGroupError : public std::exception

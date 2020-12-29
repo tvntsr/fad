dc4cd3fe707e6c3b51cfb68aa29674cbe24c6ed9
Volodymyr Tarasenko <tvntsr@yahoo.com>
2020-12-29 20:33:28 +0200

    fanotify FID support, MetadataWorker redesigned

--- src/metadataworker.cpp
+++ src/metadataworker.cpp
@@ -27,26 +27,10 @@
 #include <boost/asio/read.hpp>
 
 #include "metadataworker.hpp"
-#include "fanotifyerror.hpp"
 #include "log.hpp"
 
 namespace io = boost::asio;
 namespace fs = std::filesystem;
-
-struct AutoCloseFd
-{
-    AutoCloseFd(int fd)
-        : m_fd(fd)
-    {}
-
-    ~AutoCloseFd()
-    {
-        close(m_fd);
-    }
-    
-private:
-    int m_fd;
-};
 
 struct FanMaskMapping
 {
@@ -72,71 +56,6 @@ struct FanMaskMapping
     { FAN_OPEN_PERM,      "OPEN_PERM",      "An application wants to open a file or directory."},
     { FAN_OPEN_EXEC_PERM, "OPEN_EXEC_PERM", "An  application  wants  to  open a file for execution."}
 };
-
-MetadataWorker::DATA_RESULT
-MetadataWorker::operator()(const fanotify_event_metadata *metadata, ssize_t len, io::yield_context& yield)
-{
-    std::string uid_real_s
-        , uid_effective_s;
-    
-    LogDebug("Start operator()");
-
-    AutoCloseFd auto_fd(metadata->fd);
-    
-    // Check that run-time and compile-time structures match
-    if (metadata->vers != FANOTIFY_METADATA_VERSION)
-    {
-        throw FanotifyGroupError("mismatch of fanotify metadata version", errno);
-    }
-
-    /* metadata->fd contains either FAN_NOFD, indicating a
-       queue overflow, or a file descriptor (a nonnegative
-       integer). Here, we simply ignore queue overflow. */
-    
-    if (metadata->fd < 0)
-    {
-        throw FanotifyNoDataError();
-    }
-
-    /* Handle open permission event */
-    if (metadata->mask & FAN_OPEN_PERM)
-    {
-        LogDebug("FAN_OPEN_PERM ");
-            
-        struct fanotify_response response;
-        // Allow file to be opened 
-        response.fd = metadata->fd;
-        response.response = FAN_ALLOW;
-        write(m_notify_fd.native_handle(), &response,
-              sizeof(struct fanotify_response));
-            
-        // io::async_write(m_notify_fd,
-        //             io::buffer(&response, sizeof(struct fanotify_response)),
-        //             yield);
-        //write(fd, &response,
-        //      sizeof(struct fanotify_response));
-    }
-    std::tie(uid_real_s, uid_effective_s) = parseProc(metadata->pid, yield);
-    LogDebug("PID " << metadata->pid << ", uids:" << uid_real_s << "/" << uid_effective_s);
-    
-    auto path = getRealFilePath(metadata->fd);
-    LogDebug("Path: " << path);
-    auto type_and_comment = printAccessType(metadata);
-    // Handle closing of writable file event
-
-    if (metadata->mask & FAN_CLOSE_WRITE)
-        LogDebug("FAN_CLOSE_WRITE ");
-
-    // Close the file descriptor of the event
-    //close(metadata->fd);
-
-    return std::make_tuple(std::make_pair("UID real", uid_real_s)
-                           , std::make_pair("UID effective", uid_effective_s)
-                           , std::make_pair("PID", metadata->pid)
-                           , path.string()
-                           , type_and_comment
-        );
-}
 
 std::pair<std::string, std::string>
 MetadataWorker::printAccessType(const fanotify_event_metadata *metadata)
@@ -172,7 +91,7 @@ fs::path MetadataWorker::getRealFilePath(int fd)
     return fs::read_symlink(p);
 }
     
-std::pair<std::string, std::string> MetadataWorker::parseProc(int pid, io::yield_context& yield)
+std::tuple<std::string, std::string, std::string> MetadataWorker::parseProc(int pid, io::yield_context& yield)
 {
     LogDebug("Reading proc, pid " << pid);
     
@@ -189,6 +108,7 @@ std::pair<std::string, std::string> MetadataWorker::parseProc(int pid, io::yield
 
     LogDebug("Go " << p);
     std::string line;
+    std::string binary_name;
     boost::asio::streambuf input_buffer;
 
     boost::asio::posix::stream_descriptor file_reader(m_notify_fd.get_executor(),
@@ -206,16 +126,157 @@ std::pair<std::string, std::string> MetadataWorker::parseProc(int pid, io::yield
             for (std::string line; std::getline(is, line); )
             {
                 LogDebug("Line ["<< line << "], len " << len);
-                if (boost::starts_with(line, "Uid"))
+                if (binary_name.empty() && boost::starts_with(line, "Name"))
                 {
                     std::vector<std::string> parsed_record;
                     boost::split(parsed_record, line, boost::is_any_of(" \t"));
-                    return std::make_pair(parsed_record[1], parsed_record[2]);
+
+                    binary_name = parsed_record[1];
+                }
+                else if (boost::starts_with(line, "Uid"))
+                {
+                    std::vector<std::string> parsed_record;
+                    boost::split(parsed_record, line, boost::is_any_of(" \t"));
+                    return std::make_tuple(parsed_record[1], parsed_record[2], binary_name);
                 }
             }
         }
     }
     while(len > 0);
     
-    return std::make_pair("-", "-");
+    return std::make_tuple("-", "-", "-");
+}
+
+MetadataWorker::DATA_RESULT
+MetadataWorker::metadataParser(const fanotify_event_metadata *metadata, ssize_t len, io::yield_context& yield)
+{
+    LogDebug("Start parsing metadata");
+
+    AutoCloseFd auto_fd(metadata->fd);
+    
+    // Check that run-time and compile-time structures match
+    if (metadata->vers != FANOTIFY_METADATA_VERSION)
+    {
+        throw FanotifyGroupError("mismatch of fanotify metadata version", errno);
+    }
+
+    /* man page:
+       metadata->fd contains either FAN_NOFD, indicating a
+       queue overflow, or a file descriptor (a nonnegative
+       integer). Here, we simply ignore queue overflow. */
+    
+    if (metadata->fd < 0)
+    {
+        throw FanotifyNoDataError();
+    }
+
+    /* Handle open permission event */
+    if (metadata->mask & FAN_OPEN_PERM)
+    {
+        LogDebug("FAN_OPEN_PERM ");
+            
+        struct fanotify_response response;
+        // Allow file to be opened 
+        response.fd = metadata->fd;
+        response.response = FAN_ALLOW;
+        (void)!write(m_notify_fd.native_handle(), &response,
+                     sizeof(struct fanotify_response));
+            
+        // io::async_write(m_notify_fd,
+        //             io::buffer(&response, sizeof(struct fanotify_response)),
+        //             yield);
+        //write(fd, &response,
+        //      sizeof(struct fanotify_response));
+    }
+
+    return prepareDataResult(metadata->pid, std::move(auto_fd), printAccessType(metadata), yield);
+}
+
+MetadataWorker::DATA_RESULT
+MetadataWorker::fidMetadataParser(const fanotify_event_metadata *metadata, ssize_t len, io::yield_context& yield)
+{
+    LogDebug("Start parsing metadata and fid structure");
+   
+    // Check that run-time and compile-time structures match
+    if (metadata->vers != FANOTIFY_METADATA_VERSION)
+    {
+        throw FanotifyGroupError("mismatch of fanotify metadata version", errno);
+    }
+
+    // metadata->fd shoul contain FAN_NOFD in case of FID
+    if (metadata->fd >= 0)
+    {
+        throw FanotifyNoDataError();
+    }
+
+    // C structure, C style 
+    struct fanotify_event_info_fid* fid = (struct fanotify_event_info_fid *) (metadata + 1);
+    struct file_handle* file_handle = (struct file_handle *) fid->handle;
+
+    // Ensure that the event info is of the correct type
+    if (fid->hdr.info_type != FAN_EVENT_INFO_TYPE_FID)
+    {
+        throw FanotifyGroupError("unexpected event info type", errno);
+    }
+
+    auto type_and_comment = printAccessType(metadata);
+    
+    /* according to man page:
+       metadata->fd is set to FAN_NOFD when FAN_REPORT_FID is enabled.
+       To obtain a file descriptor for the file object corresponding to
+       an event you can use the struct file_handle that's provided
+       within the fanotify_event_info_fid in conjunction with the
+       open_by_handle_at(2) system call. A check for ESTALE is done
+       to accommodate for the situation where the file handle for the
+       object was deleted prior to this system call. */
+
+    LogDebug("fsid.val[0]: " << fid->fsid.val[0] << ", fsid.val[1]: " << fid->fsid.val[1]);
+    
+    int event_fd = open_by_handle_at(AT_FDCWD, file_handle, O_RDONLY);
+    if (event_fd == -1) {
+        if (errno == ESTALE) {
+            LogWarning("File pointed by file handle has been deleted");
+            throw FanotifyNoDataError();
+        } else {
+            throw FanotifyGroupError("Error with open_by_handle_at", errno);
+        }
+    }
+   
+    return prepareDataResult(metadata->pid, AutoCloseFd(event_fd), type_and_comment, yield);
+}
+
+MetadataWorker::DATA_RESULT
+MetadataWorker::prepareDataResult(int pid,
+                                  AutoCloseFd&& auto_fd,
+                                  const std::pair<std::string, std::string>& type_and_comment,
+                                  io::yield_context& yield)
+{
+    AutoCloseFd fd(std::move(auto_fd));
+
+    std::string uid_real_s, uid_effective_s, binary_name;
+
+    try
+    {
+        std::tie(uid_real_s, uid_effective_s, binary_name) = parseProc(pid, yield);
+        LogDebug("PID " << pid << "("<< binary_name << "), uids:" << uid_real_s << "/" << uid_effective_s);
+    }
+    catch(FanotifyNoDataError& err)
+    {
+        LogWarning("Cannot get uid, proc file already removed");
+    }
+    catch(FanotifyGroupError& err)
+    {
+        LogWarning("Cannot get uid, " << err.what());
+    }
+    
+    auto path = getRealFilePath(fd.getFd());
+    LogDebug("Path: " << path);
+
+    return std::make_tuple(std::make_pair("UID real", uid_real_s)
+                           , std::make_pair("UID effective", uid_effective_s)
+                           , std::make_pair("PID", pid)
+                           , binary_name
+                           , path.string()
+                           , type_and_comment
+        );
 }
--- src/metadataworker.hpp
+++ src/metadataworker.hpp
@@ -18,6 +18,8 @@
 //     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <sys/fanotify.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include <tuple>
 #include <string>
@@ -26,12 +28,16 @@
 #include <boost/asio/posix/stream_descriptor.hpp>
 #include <boost/asio/spawn.hpp>
 
+#include "fanotifyerror.hpp"
+#include "autoclosefd.hpp"
+
 class MetadataWorker
 {
 public:
     typedef std::tuple<std::pair<std::string, std::string>,
                        std::pair<std::string, std::string>,
                        std::pair<std::string, int>,
+                       std::string,
                        std::string,
                        std::pair<std::string, std::string>>
     DATA_RESULT;
@@ -40,22 +46,46 @@ public:
 public:
     MetadataWorker(boost::asio::posix::stream_descriptor& notify_fd)
         : m_notify_fd(notify_fd)
-    {}
+        , m_pid(getpid())
+    {
+
+    }
 
     // uid real, uid effective, pid, access type, comment
     DATA_RESULT
-    operator()(const fanotify_event_metadata *metadata, ssize_t len, boost::asio::yield_context& yield);
+    operator()(const fanotify_event_metadata *metadata, ssize_t len, boost::asio::yield_context& yield)
+    {
+        if (metadata->pid == m_pid)
+            throw FanotifyNoDataError(); // skip myself
+
+        return (metadata->event_len == FAN_EVENT_METADATA_LEN) ?
+            metadataParser(metadata, len, yield) :
+            fidMetadataParser(metadata, len, yield);
+    }
 
 private:
     std::pair<std::string, std::string> printAccessType(const fanotify_event_metadata *metadata);
     
     std::filesystem::path getRealFilePath(int fd);
     
-    std::pair<std::string, std::string> parseProc(int pid, boost::asio::yield_context& yield);
-   
+    std::tuple<std::string, std::string, std::string>
+    parseProc(int pid, boost::asio::yield_context& yield);
+
+    DATA_RESULT
+    metadataParser(const fanotify_event_metadata *metadata, ssize_t len, boost::asio::yield_context& yield);
+
+    DATA_RESULT
+    fidMetadataParser(const fanotify_event_metadata *metadata, ssize_t len, boost::asio::yield_context& yield);
+
+    DATA_RESULT
+    prepareDataResult(int pid,
+                      AutoCloseFd&& auto_fd,
+                      const std::pair<std::string, std::string>& type_and_comment,
+                      boost::asio::yield_context& yield);
 
 private:
     boost::asio::posix::stream_descriptor& m_notify_fd;
+    int m_pid; // fanotify pid is int, so make it int as well
 };
 
 #endif //_FAD_META_DATA_WORKER_HPP
